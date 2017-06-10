36ab3b2a772b15bd30c4b9766158ee19b8aae2f2
MargarethaAnne <margaretha.anne.haughwout@gmail.com>
2017-06-09 22:04:13 -0500

    add arduino code

---
+++ morse_ard_code_for_bees/morse_ard_code_for_bees.ino
@@ -0,0 +1,416 @@
+
+int led = 0;
+
+
+void setup() {
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  
+  delay(2000);
+
+/* uncomment one of these functions below and send to Arduino/ ATTiny */
+
+  //apple();
+  apricot();
+  //cherry();
+  //nectarine();
+  //peach();
+    //plum();
+  //pluot();
+
+  delay(2000);
+
+}
+
+
+void apple() {
+  // A  • –
+
+  digitalWrite(led, HIGH);
+  delay(150);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  // P• – – •
+  digitalWrite(led, HIGH);
+  delay(150);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(150);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  // P• – – •
+  digitalWrite(led, HIGH);
+  delay(150);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(150);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  // L • – • •
+
+  digitalWrite(led, HIGH);
+  delay(150);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(150);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(150);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  // E •
+
+  digitalWrite(led, HIGH);
+  delay(150);
+  digitalWrite(led, LOW);
+  delay(150);
+}
+void apricot() {
+  // A  • –
+
+  digitalWrite(led, HIGH);
+  delay(150);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  // P• – – •
+  digitalWrite(led, HIGH);
+  delay(150);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(150);
+  digitalWrite(led, LOW);
+  delay(150);
+  // R • – •
+
+  digitalWrite(led, HIGH);
+  delay(150);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(150);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  // I • •
+
+  digitalWrite(led, HIGH);
+  delay(150);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(150);
+  digitalWrite(led, LOW);
+  delay(150);
+
+
+  // C – • – •
+
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(150);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(150);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  // O – – –
+
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  // T –
+
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(150);
+}
+
+void cherry() {
+  // C – • – •
+
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(150);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(150);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  // H • • • • 
+
+  digitalWrite(led, HIGH);
+  delay(150);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(150);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(150);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(150);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  // E •
+
+  digitalWrite(led, HIGH);
+  delay(150);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  // R • – •
+
+  digitalWrite(led, HIGH);
+  delay(150);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(150);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  // R • – •
+
+  digitalWrite(led, HIGH);
+  delay(150);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(150);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  // Y – • – –
+
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(150);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(150);
+
+}
+
+void nectarine() {
+
+}
+
+void peach() {
+
+}
+
+void plum() {
+  // P• – – •
+  digitalWrite(led, HIGH);
+  delay(150);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(150);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  // L • – • •
+
+  digitalWrite(led, HIGH);
+  delay(150);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(150);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(150);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  // U • • –
+
+  digitalWrite(led, HIGH);
+  delay(150);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(150);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  // M – –
+
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(150);
+
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(150);
+
+}
+
+
+
